ab355da5049508a651cf63e36ddd3c9da3929975
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-23 05:04:10 -0500

    API URL cambio a local

--- src/app/person/person.service.ts
+++ src/app/person/person.service.ts
@@ -10,7 +10,7 @@ import { Person } from './person';
   providedIn: 'root',
 })
 export class PersonService {
-  private apiURL = 'https://sheltered-caverns-32656.herokuapp.com/api/person/';
+  private apiURL = 'https://localhost:8000/api/person/';
 
   httpOptions = {
     headers: new HttpHeaders({
