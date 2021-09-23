c7e83b961e39f40c93bbb83bacc949bd03782009
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-23 02:22:51 -0500

    Validando formulario y creando persona

--- src/app/person/create/create.component.ts
+++ src/app/person/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonService } from '../person.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    public personService: PersonService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+
+    this.form = new FormGroup({ 
+      name:  new FormControl('', [ Validators.required, Validators.pattern('^[a-zA-ZÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÑñüÜ \-\']+') ]),
+      email: new FormControl('', [ Validators.required, Validators.email ]),
+      phone: new FormControl('', [ Validators.required, Validators.pattern("^[0-9]*$") ])
+    });
+
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    console.log(this.form.value);
+    this.personService.create(this.form.value).subscribe(res => {
+         console.log('Person created successfully!');
+         this.router.navigateByUrl('person/index');
+    })
   }
 
 }
